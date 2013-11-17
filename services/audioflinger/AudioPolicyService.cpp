39cb8073b1dacdcc71107d1663f186f5f9e5976e
Menno van Grinsven <scanno71@gmail.com>
2013-11-17 13:24:01 +0100

    Support pre-KitKat audio blobs
    Change-Id: Ibd6a12ca02684e0f931489a44b898a62d4364753

--- services/audioflinger/AudioPolicyService.cpp
+++ services/audioflinger/AudioPolicyService.cpp
@@ -1145,6 +1145,10 @@ int AudioPolicyService::setVoiceVolume(float volume, int delayMs)
 
 bool AudioPolicyService::isOffloadSupported(const audio_offload_info_t& info)
 {
+#ifdef HAVE_PRE_KITKAT_AUDIO_BLOB
+    return false;
+#endif
+
     if (mpAudioPolicy == NULL) {
         ALOGV("mpAudioPolicy == NULL");
         return false;
